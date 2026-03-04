d95c950194e72089e566d1a3695c5f604754f997
Bhavesh Varma <bhavesh.236457101@vcet.edu.in>
2026-03-05 04:46:07 +0530

    sitemap generation

--- sites/vestacall/astro.config.mjs
+++ sites/vestacall/astro.config.mjs
@@ -23,6 +23,31 @@ export default defineConfig({
       JavaScript: true,
       Image: true,
     }),
+    {
+      name: 'sitemap-generator',
+      hooks: {
+        'astro:build:done': async ({ dir, pages }) => {
+          const fs = await import('node:fs/promises');
+          const siteUrl = 'https://vestacall.com';
+
+          let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+          xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+
+          for (const page of pages) {
+            // Exclude 404 page
+            if (page.pathname.includes('404')) continue;
+
+            const loc = new URL(page.pathname, siteUrl).href.replace(/\/$/, '') + '/';
+            xml += `  <url>\n    <loc>${loc}</loc>\n  </url>\n`;
+          }
+
+          xml += '</urlset>';
+
+          await fs.writeFile(new URL('sitemap.xml', dir), xml);
+          console.log('✅ sitemap.xml generated successfully.');
+        }
+      }
+    }
   ],
   build: {
     inlineStylesheets: 'always',
